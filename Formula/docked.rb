9f1763982f90fde2d0d875cb667ea1388340788d
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-14 07:57:26 -0400

    Brew formula update for docked version v0.3.2

--- Formula/docked.rb
+++ Formula/docked.rb
@@ -5,33 +5,33 @@
 class Docked < Formula
   desc "A Dockerfile linter"
   homepage "https://github.com/jimschubert/docked"
-  version "0.3.1"
+  version "0.3.2"
   license "Apache 2.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/jimschubert/docked/releases/download/v0.3.1/docked_0.3.1_Darwin_x86_64.tar.gz"
-      sha256 "97b136b8e9617c3f72e4e793b99fd0051d4579acbb64300f12ba1433b0af5fcd"
+      url "https://github.com/jimschubert/docked/releases/download/v0.3.2/docked_0.3.2_Darwin_x86_64.tar.gz"
+      sha256 "f43b893a6f5b66341ab440dba432e72e141d018292545283434c282c49019a89"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/jimschubert/docked/releases/download/v0.3.1/docked_0.3.1_Darwin_arm64.tar.gz"
-      sha256 "8bec2ca555bb2bf480ffcdb494863f1207f996662ab6c22ee60c4f11dbd8de58"
+      url "https://github.com/jimschubert/docked/releases/download/v0.3.2/docked_0.3.2_Darwin_arm64.tar.gz"
+      sha256 "61de8c67165f602085312a567c207e2f097b556d4f16a5372b011b2ff7eefcfe"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/jimschubert/docked/releases/download/v0.3.1/docked_0.3.1_Linux_x86_64.tar.gz"
-      sha256 "c803e34472b844f61f42406adbea51231d7a1a9eaa186498056db353d2fd20b2"
+      url "https://github.com/jimschubert/docked/releases/download/v0.3.2/docked_0.3.2_Linux_x86_64.tar.gz"
+      sha256 "c9ebf2f80d2dab5cf37cc0898ed620fd5592b8d03862d32f7557299685ae6d99"
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/jimschubert/docked/releases/download/v0.3.1/docked_0.3.1_Linux_armv6.tar.gz"
-      sha256 "be0a0bfa07903a56eb40075f6739477c46205f5a20fd691f71663eb0fa6b277d"
+      url "https://github.com/jimschubert/docked/releases/download/v0.3.2/docked_0.3.2_Linux_armv6.tar.gz"
+      sha256 "f1ba7805d725823a7918feaadfccdab3ce1685ac7af35acda7be3294ef8100d4"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/jimschubert/docked/releases/download/v0.3.1/docked_0.3.1_Linux_arm64.tar.gz"
-      sha256 "03bbbd2fb565a902ed9825316e58ef8a4169f8c7efdcff541978d44b98d9fddc"
+      url "https://github.com/jimschubert/docked/releases/download/v0.3.2/docked_0.3.2_Linux_arm64.tar.gz"
+      sha256 "f6d2aecf81ace3e87478a6a58f74aa669430305f531f4f9f29c02269316cefa0"
     end
   end
 
